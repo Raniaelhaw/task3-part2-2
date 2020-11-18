fa081340a905c9a5aa1d48086f9fe3858621c61c
someone <someone@someplace.com>
2020-11-18 20:39:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './components/Home';
-
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import {Switch,Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import NotFoundPage from './components/pages/NotFoundPage';
+import List from './components/List';
+import PersonInput from './components/PersonInput';
 function App() {
   return (
     <div>
     <Navbar/>
+    <Switch> 
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      <Route component={NotFoundPage}/>
+    </Switch>
     <br/>
-    <Home />
+    <center>
+      <PersonInput/>
+    <List/>
+    </center>
+    
     <br />
     <br />
     <br />
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from '../logo.svg';
+import { Link} from 'react-router-dom';
 
  function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <a className="navbar-brand" href="#">
-  
-    
-       
-                </a>
+        
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span>
             <i className="fas fa-bars" style={{ color:'#fff'}}/>
@@ -18,13 +14,14 @@ import logo from '../logo.svg';
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav m-auto">
             <li className="nav-item active">
-              <a className="nav-link text-white text-uppercase ml-5" href="#">Home&nbsp;<i class="fas fa-home"></i> <span class="sr-only">(current)</span></a>
+              <Link  className="nav-link text-white text-uppercase ml-5" to="/">Home&nbsp;<i class="fas fa-home"></i> 
+              <span className="sr-only">(current)</span></Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white text-uppercase ml-5" href="#">News</a>
+              <Link  className="nav-link text-white text-uppercase ml-5" to="about">ABOUT US</Link >
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white text-uppercase ml-5" href="#">Contact us</a>
+              <Link  className="nav-link text-white text-uppercase ml-5" to="contact">Contact us</Link >
             </li>
            
             
